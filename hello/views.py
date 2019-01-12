348e1e25d5e77d258ee8785f30368165d97e18fe
sadavar <sadavar05@gmail.com>
2019-01-12 18:11:06 -0500

    Version 1 - added news

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
+import praw
 from .models import Greeting
+
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+
+    reddit = praw.Reddit(client_id='PqeRHaJrQIdEQg', client_secret='rtA7jHjPB9x3cpDsaXKES-G2Tt8', user_agent='Linux:TopTopics:v0.0.1 (by /u/ModsAreKindaGay)')
+    topics = []
+    topics2 = []
+    for submission in reddit.subreddit('technews').hot(limit=10):
+    	topics.append(submission.title)
+
+    for number, topic in enumerate(topics, 1):
+    	topics2.append(str(number) + '. ' + topic)
+
+
+	#for number, topic in enumerate(topics, 1):
+	#	print(str(number) + '.', topic)
+
+    return HttpResponse('<pre>' + '<h1 style="color:black; text-align:center; font-size: 50px;"> TopTopics: Tech News</h1>' + str(topics2[0]) + "\n" +  "\n" + str(topics2[1]) + "\n" + "\n" + str(topics2[2]) + "\n" + "\n" + str(topics2[3]) + "\n" + "\n" + str(topics2[4]) + "\n" + "\n" + str(topics2[5]) + "\n" + "\n" + str(topics2[6]) + "\n" + "\n" + str(topics2[7]) + "\n" + "\n" + str(topics2[8]) + "\n" + "\n" + str(topics2[9]) + '</pre>')
 
 
 def db(request):
