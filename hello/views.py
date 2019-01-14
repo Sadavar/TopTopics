a0bcc1ac1a4f549c26269b6782c544a03089cd3c
sadavar <sadavar05@gmail.com>
2019-01-13 20:21:58 -0500

    added links to the articles

--- hello/views.py
+++ hello/views.py
@@ -10,11 +10,20 @@ from .models import Greeting
 def index(request):
 
     reddit = praw.Reddit(client_id='PqeRHaJrQIdEQg', client_secret='rtA7jHjPB9x3cpDsaXKES-G2Tt8', user_agent='Linux:TopTopics:v0.0.1 (by /u/ModsAreKindaGay)')
-    topic_list1 = []
-    for submission in reddit.subreddit('technews').hot(limit=12):
-    	topic_list1.append(submission.title)
+    topic_list = []
+    topic_urls = []
+    topic_and_url = {}
+    x = 0
 
-    return render(request, 'index.html', {'topics': topic_list1})
+    for submission in reddit.subreddit('technews').hot(limit=12):
+        topic_list.append(submission.title)
+        topic_urls.append(submission.url)
+        topic_and_url[topic_list[x]] = topic_urls[x]
+        x += 1
+
+    #return render(request, 'index.html', {'topics': topic_and_url})
+    return render(request, 'index.html', {"topic_and_url": topic_and_url})
+
 
 def db(request):
 
